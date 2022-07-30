530ae0f339dd4e2746fc391c872da134817aa562
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-07-30 00:24:51 -0700

    creating react app
    created react app, added comments and imported apiKey. Added folders to github

---
+++ app/src/App.js
@@ -0,0 +1,31 @@
+//This file serves as my main container component
+
+import logo from './logo.svg';
+import './App.css';
+import apiKey from './config';
+import './config.js';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
